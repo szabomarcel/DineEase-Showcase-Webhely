ccc7b6100e22cff85c5e3569eb7d7352d80b520b
szabomarcel <szabomarcell34@gmail.com>
2024-02-28 19:31:56 +0100

    oraimunka

--- web-component/js/web.js
+++ web-component/js/web.js
@@ -14,7 +14,7 @@ function displayRestaurants(restaurants) {
   restaurants.forEach(restaurant => {
       const restaurantDiv = document.createElement('div');
       restaurantDiv.innerHTML = `
-      <img src="data\\${restaurant.image}">
+      <img src="data\\${restaurant.image}" style="width: 390px; height: 200px;">
       <h4 class="card-title d-flex justify-content-between"><span
           class="m-4"><b>${restaurant.name}</b></span><span
           class="m-4"><img src="picture/images/star.png"
@@ -32,6 +32,17 @@ function displayRestaurants(restaurants) {
           <p><strong></strong> ${restaurant.description}</p>
           <a href="#">View restaurant</a>
     `;
-      restaurantsListDiv.appendChild(restaurantDiv);
+      restaurantsListDiv.appendChild(restaurantDiv);    
   });
 }
+
+const app = new Vue({
+    el: '#app',
+    data: {
+      events: [
+        { id: 1, title: 'Event 1', description: 'Description for Event 1', date: 'Date 1' },
+        { id: 2, title: 'Event 2', description: 'Description for Event 2', date: 'Date 2' },
+        // Add more events as needed
+      ]
+    }
+  });
